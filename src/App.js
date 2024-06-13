5799822b64f736f0989eff08856928a1f78491c6
estefaniaperezz <166597108+estefaniaperezz@users.noreply.github.com>
2024-06-13 14:23:40 +0200

    barra navegacion enlaces

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes, Link, NavLink } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import './index.css'; // Asegúrate de que el archivo CSS esté importado
 import Services from './components/Services';
-import OurTeam from './components/OurTeam'; // Importa el componente OurTeam
+import OurTeam from './components/OurTeam';
+import CitaPrevia from './components/CitaPrevia';
+import Urgencias from './components/Urgencias';
+import Contacto from './components/Contacto';
 
 function App() {
   return (
-    <div style={{ backgroundColor: '#fff4e1' }}> {/* Ajuste del color de fondo */}
-      <NavBar />
-      <div className="hero-container position-relative">
-        <img src="/fondo.png" alt="Imagen de fondo" className="img-fluid w-100" />
-        <div className="overlay-content text-center">
-          <h1>Clínica Veterinaria Por Ellos</h1>
-          <p>Bienvenidos a Clínica Veterinaria Por Ellos, donde el bienestar de tu mascota es nuestra misión. Cada mascota que atendemos es tratada con la dedicación y el cariño que se merece. Reserva tu cita aquí y confía en nuestro equipo para mantener a tu mascota sana y feliz. Tu tranquilidad es nuestra prioridad.</p>
-          <button className="btn btn-primary">Pide Cita</button>
-        </div>
+    <BrowserRouter>
+      <div style={{ backgroundColor: '#fff4e1' }}>
+        <NavBar>
+          {/* Enlaces de navegación */}
+          <NavLink to="/" className="nav-link" activeClassName="active" exact>Inicio</NavLink>
+          <NavLink to="/servicios" className="nav-link" activeClassName="active">Nuestros Servicios</NavLink>
+          <NavLink to="/contacto" className="nav-link" activeClassName="active">Contacto</NavLink>
+        </NavBar>
+
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <div className="hero-container position-relative">
+                <img src="/fondo.png" alt="Imagen de fondo" className="img-fluid w-100" />
+                <div className="overlay-content text-center">
+                  <h1>Clínica Veterinaria Por Ellos</h1>
+                  <p>Bienvenidos a Clínica Veterinaria Por Ellos, donde el bienestar de tu mascota es nuestra misión. Cada mascota que atendemos es tratada con la dedicación y el cariño que se merece. Reserva tu cita aquí y confía en nuestro equipo para mantener a tu mascota sana y feliz. Tu tranquilidad es nuestra prioridad.</p>
+                  <button className="btn btn-primary">Pide Cita</button>
+                </div>
+              </div>
+              <Services />
+              <OurTeam />
+            </div>
+          } />
+          <Route path="/servicios" element={<Services />} />
+          <Route path="/cita-previa" element={<CitaPrevia />} />
+          <Route path="/urgencias" element={<Urgencias />} />
+          <Route path="/contacto" element={<Contacto />} />
+        </Routes>
       </div>
-      <Services />
-      <OurTeam />
-    </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FaFacebookSquare, FaInstagramSquare } from 'react-icons/fa'; // Importa los íconos de FontAwesome
+import { FaFacebookSquare, FaInstagramSquare } from 'react-icons/fa';
 
 function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
-      <a className="navbar-brand text-dark" href="#">Clínica veterinaria Por ellos</a>
+      <Link className="navbar-brand text-dark" to="/">Clínica veterinaria Por ellos</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link" href="#servicios">Nuestros Servicios</a>
+            <Link className="nav-link" to="/servicios">Nuestros Servicios</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#cita-previa">Cita Previa</a>
+            <Link className="nav-link" to="/cita-previa">Cita Previa</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#urgencias">Urgencias</a>
+            <Link className="nav-link" to="/urgencias">Urgencias</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#contacto">Contacto</a>
+            <Link className="nav-link" to="/contacto">Contacto</Link>
           </li>
         </ul>
         <ul className="navbar-nav ml-3">
           <li className="nav-item">
             <a className="nav-link" href="">
-              <FaFacebookSquare style={{ fontSize: '24px' }} /> {/* Ajusta el tamaño del ícono */}
+              <FaFacebookSquare style={{ fontSize: '24px' }} />
             </a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="">
-              <FaInstagramSquare style={{ fontSize: '24px' }} /> {/* Ajusta el tamaño del ícono */}
+              <FaInstagramSquare style={{ fontSize: '24px' }} />
             </a>
           </li>
         </ul>
