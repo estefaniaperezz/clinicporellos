1731cc075ce92d4195e3975a4b6ad3202faf7caf
estefaniaperezz <166597108+estefaniaperezz@users.noreply.github.com>
2024-06-13 16:06:40 +0200

    footer.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Services from './components/Services';
 import OurTeam from './components/OurTeam';
 import CitaPrevia from './components/CitaPrevia';
 import Urgencias from './components/Urgencias';
 import Contacto from './components/Contacto';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
-      <div style={{ backgroundColor: '#B5D0CC' }}>
+      <div style={{ backgroundColor: '#B5D0CC', minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
         <NavBar>
-          {/* Enlaces de navegación */}
           <NavLink to="/" className="nav-link" activeClassName="active" exact>Inicio</NavLink>
           <NavLink to="/servicios" className="nav-link" activeClassName="active">Nuestros Servicios</NavLink>
           <NavLink to="/contacto" className="nav-link" activeClassName="active">Contacto</NavLink>
         </NavBar>
-
-        <Routes>
-          <Route path="/" element={
-            <div>
-              <div className="hero-container position-relative">
-                <img src="/fondo.png" alt="Imagen de fondo" className="img-fluid w-100" />
-                <div className="overlay-content text-center">
-                  <h1>Clínica Veterinaria Por Ellos</h1>
-                  <p>Bienvenidos a Clínica Veterinaria Por Ellos, donde el bienestar de tu mascota es nuestra misión. Cada mascota que atendemos es tratada con la dedicación y el cariño que se merece. Reserva tu cita aquí y confía en nuestro equipo para mantener a tu mascota sana y feliz. Tu tranquilidad es nuestra prioridad.</p>
-                  <button className="btn btn-primary">Pide Cita</button>
+        <div style={{ flex: '1' }}>
+          <Routes>
+            <Route path="/" element={
+              <div>
+                <div className="hero-container position-relative">
+                  <img src="/fondo.png" alt="Imagen de fondo" className="img-fluid w-100" />
+                  <div className="overlay-content text-center">
+                    <h1>Clínica Veterinaria Por Ellos</h1>
+                    <p>Bienvenidos a Clínica Veterinaria Por Ellos, donde el bienestar de tu mascota es nuestra misión. Cada mascota que atendemos es tratada con la dedicación y el cariño que se merece. Reserva tu cita aquí y confía en nuestro equipo para mantener a tu mascota sana y feliz. Tu tranquilidad es nuestra prioridad.</p>
+                    <button className="btn btn-primary">Pide Cita</button>
+                  </div>
                 </div>
+                <Services />
+                <OurTeam />
               </div>
-              <Services />
-              <OurTeam />
-            </div>
-          } />
-          <Route path="/servicios" element={<Services />} />
-          <Route path="/cita-previa" element={<CitaPrevia />} />
-          <Route path="/urgencias" element={<Urgencias />} />
-          <Route path="/contacto" element={<Contacto />} />
-        </Routes>
+            } />
+            <Route path="/servicios" element={<Services />} />
+            <Route path="/cita-previa" element={<CitaPrevia />} />
+            <Route path="/urgencias" element={<Urgencias />} />
+            <Route path="/contacto" element={<Contacto />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../index.css'; // Importa el archivo CSS para estilos del footer
+import { FaFacebookSquare, FaInstagramSquare } from 'react-icons/fa';
+
+
+function Footer() {
+  return (
+    <footer className="footer footer-container">
+      <div className="logo-container">
+        <img src="/logo.png" alt="Logo" className="logo-img" />
+      </div>
+      <div className="info-container">
+        <p>&copy; Por Ellos. Todos los derechos reservados.</p>
+        <p>Diseñado por Estefanía Pérez</p>
+      </div>
+      <div className="social-container">
+        <a href="https://www.facebook.com/tu_facebook"><FaFacebookSquare style={{ fontSize: '30px' }} /></a>
+        <a href="https://www.instagram.com/tu_instagram"><FaInstagramSquare style={{ fontSize: '30px' }} /></a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+
+
+
+
+
+
+
