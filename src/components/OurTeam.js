65070e63c1290edaa7c9fb62572fb7f79b4c0c27
estefaniaperezz <166597108+estefaniaperezz@users.noreply.github.com>
2024-06-13 12:50:43 +0200

    ourteam.js

--- src/components/OurTeam.js
+++ src/components/OurTeam.js
@@ -7,23 +7,23 @@ function OurTeam() {
       <div className="row justify-content-center">
         <div className="col-md-3 mb-4 text-center">
           <img src="/veterinario1.jpg" alt="Persona 1" className="img-fluid rounded-circle mb-3 team-member-img" style={{ width: '400px', height: '250px' }} />
-          <p className="fw-bold">Nombre 1</p>
-          <p className="text-muted">Cargo</p>
+          <p className="fw-bold">Irene Muñoz</p>
+          <p className="text-muted">Gerente/Veterinaria</p>
         </div>
         <div className="col-md-3 mb-4 text-center">
           <img src="/veterinario2.jpg" alt="Persona 2" className="img-fluid rounded-circle mb-3 team-member-img" style={{ width: '400px', height: '250px' }} />
-          <p className="fw-bold">Nombre 2</p>
-          <p className="text-muted">Cargo</p>
+          <p className="fw-bold">Josete García</p>
+          <p className="text-muted">Cirujano</p>
         </div>
         <div className="col-md-3 mb-4 text-center">
           <img src="/veterinario3.jpg" alt="Persona 3" className="img-fluid rounded-circle mb-3 team-member-img" style={{ width: '400px', height: '250px' }} />
-          <p className="fw-bold">Nombre 3</p>
-          <p className="text-muted">Cargo</p>
+          <p className="fw-bold">Trinidad Plaza</p>
+          <p className="text-muted">Recepcionista</p>
         </div>
         <div className="col-md-3 mb-4 text-center">
           <img src="/veterinario4.jpg" alt="Persona 4" className="img-fluid rounded-circle mb-3 team-member-img" style={{ width: '400px', height: '250px' }} />
-          <p className="fw-bold">Nombre 4</p>
-          <p className="text-muted">Cargo</p>
+          <p className="fw-bold">Lolita Martínez</p>
+          <p className="text-muted">Peluquera</p>
         </div>
       </div>
     </div>
