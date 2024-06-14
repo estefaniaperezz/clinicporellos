027cdfccec677c78471bccd3bd7a8da2fa9af1a8
estefaniaperezz <166597108+estefaniaperezz@users.noreply.github.com>
2024-06-14 14:59:40 +0200

    nav redes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaFacebookSquare, FaInstagramSquare } from 'react-icons/fa';
 import { Navbar, Nav } from 'react-bootstrap';
-import '../index.css'; // Archivo para tus estilos personalizados
+import '../index.css'; // Importa tu archivo CSS personalizado
 
 function NavBar() {
   const [expanded, setExpanded] = useState(false);
@@ -22,7 +22,7 @@ function NavBar() {
           <Nav.Link as={Link} to="/urgencias" className="custom-nav-link btn mb-2 mb-lg-0" onClick={handleNavCollapse}>Urgencias</Nav.Link>
           <Nav.Link as={Link} to="/contacto" className="custom-nav-link btn mb-2 mb-lg-0" onClick={handleNavCollapse}>Contacto</Nav.Link>
         </Nav>
-        <Nav className="ml-3 align-items-center">
+        <Nav className="ml-lg-3 align-items-center"> {/* Ajusta la separación solo en dispositivos grandes */}
           <Nav.Link href="#" className="custom-nav-link btn p-2">
             <FaFacebookSquare style={{ fontSize: '24px' }} />
           </Nav.Link>
