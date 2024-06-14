78110860575330dd211e2fe322f7649266943747
estefaniaperezz <166597108+estefaniaperezz@users.noreply.github.com>
2024-06-14 14:30:25 +0200

    navbar.js
    responsive arreglado

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,44 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { FaFacebookSquare, FaInstagramSquare } from 'react-icons/fa';
+import { Navbar, Nav } from 'react-bootstrap';
+import '../index.css'; // Archivo para tus estilos personalizados
 
 function NavBar() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavCollapse = () => {
+    setExpanded(false); // Cierra la barra de navegación al hacer clic en un enlace
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light">
-      <Link className="navbar-brand text-dark" to="/">Clínica veterinaria Por ellos</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/servicios">Nuestros Servicios</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/cita-previa">Cita Previa</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/urgencias">Urgencias</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contacto">Contacto</Link>
-          </li>
-        </ul>
-        <ul className="navbar-nav ml-3">
-          <li className="nav-item">
-            <a className="nav-link" href="">
-              <FaFacebookSquare style={{ fontSize: '24px' }} />
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="">
-              <FaInstagramSquare style={{ fontSize: '24px' }} />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Navbar expand="lg" className="custom-navbar navbar-light" expanded={expanded}>
+      <Navbar.Brand as={Link} to="/" className="custom-navbar-brand text-dark">Clínica veterinaria Por ellos</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarNav" onClick={() => setExpanded(!expanded)} />
+      <Navbar.Collapse id="navbarNav" className="justify-content-end">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/servicios" className="custom-nav-link btn" onClick={handleNavCollapse}>Nuestros Servicios</Nav.Link>
+          <Nav.Link as={Link} to="/cita-previa" className="custom-nav-link btn" onClick={handleNavCollapse}>Cita Previa</Nav.Link>
+          <Nav.Link as={Link} to="/urgencias" className="custom-nav-link btn" onClick={handleNavCollapse}>Urgencias</Nav.Link>
+          <Nav.Link as={Link} to="/contacto" className="custom-nav-link btn" onClick={handleNavCollapse}>Contacto</Nav.Link>
+        </Nav>
+        <Nav className="ml-3">
+          <Nav.Link href="#" className="custom-nav-link btn">
+            <FaFacebookSquare style={{ fontSize: '24px' }} />
+          </Nav.Link>
+          <Nav.Link href="#" className="custom-nav-link btn">
+            <FaInstagramSquare style={{ fontSize: '24px' }} />
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
