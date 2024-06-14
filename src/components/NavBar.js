a0774c2c31b5547d6245750f3ade0ede327d138e
estefaniaperezz <166597108+estefaniaperezz@users.noreply.github.com>
2024-06-14 14:36:15 +0200

    navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,20 +13,20 @@ function NavBar() {
 
   return (
     <Navbar expand="lg" className="custom-navbar navbar-light" expanded={expanded}>
-      <Navbar.Brand as={Link} to="/" className="custom-navbar-brand text-dark">Clínica veterinaria Por ellos</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/" className="custom-navbar-brand text-dark mb-2 mb-lg-0">Clínica veterinaria Por ellos</Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarNav" onClick={() => setExpanded(!expanded)} />
       <Navbar.Collapse id="navbarNav" className="justify-content-end">
         <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/servicios" className="custom-nav-link btn" onClick={handleNavCollapse}>Nuestros Servicios</Nav.Link>
-          <Nav.Link as={Link} to="/cita-previa" className="custom-nav-link btn" onClick={handleNavCollapse}>Cita Previa</Nav.Link>
-          <Nav.Link as={Link} to="/urgencias" className="custom-nav-link btn" onClick={handleNavCollapse}>Urgencias</Nav.Link>
-          <Nav.Link as={Link} to="/contacto" className="custom-nav-link btn" onClick={handleNavCollapse}>Contacto</Nav.Link>
+          <Nav.Link as={Link} to="/servicios" className="custom-nav-link btn mb-2 mb-lg-0" onClick={handleNavCollapse}>Nuestros Servicios</Nav.Link>
+          <Nav.Link as={Link} to="/cita-previa" className="custom-nav-link btn mb-2 mb-lg-0" onClick={handleNavCollapse}>Cita Previa</Nav.Link>
+          <Nav.Link as={Link} to="/urgencias" className="custom-nav-link btn mb-2 mb-lg-0" onClick={handleNavCollapse}>Urgencias</Nav.Link>
+          <Nav.Link as={Link} to="/contacto" className="custom-nav-link btn mb-2 mb-lg-0" onClick={handleNavCollapse}>Contacto</Nav.Link>
         </Nav>
-        <Nav className="ml-3">
-          <Nav.Link href="#" className="custom-nav-link btn">
+        <Nav className="ml-3 align-items-center">
+          <Nav.Link href="#" className="custom-nav-link btn p-2">
             <FaFacebookSquare style={{ fontSize: '24px' }} />
           </Nav.Link>
-          <Nav.Link href="#" className="custom-nav-link btn">
+          <Nav.Link href="#" className="custom-nav-link btn p-2">
             <FaInstagramSquare style={{ fontSize: '24px' }} />
           </Nav.Link>
         </Nav>
