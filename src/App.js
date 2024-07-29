9510ede2ae5693987c6b3857e42f38bf9352e923
estefaniaperezz <166597108+estefaniaperezz@users.noreply.github.com>
2024-07-29 12:29:44 +0200

    app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, NavLink, Link } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Services from './components/Services';
 import OurTeam from './components/OurTeam';
@@ -8,44 +8,44 @@ import CitaPrevia from './components/CitaPrevia';
 import Urgencias from './components/Urgencias';
 import Peluqueria from './components/Peluqueria';
 import Footer from './components/Footer';
-import { Link } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
-      <div style={{ backgroundColor: '#BCCCE1', minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-        <NavBar>
-          <NavLink to="/" className="nav-link" activeClassName="active" exact>Inicio</NavLink>
-          <NavLink to="/servicios" className="nav-link" activeClassName="active">Nuestros Servicios</NavLink>
-          <NavLink to="/Peluqueria" className="nav-link" activeClassName="active">Peluquería</NavLink>
-        </NavBar>
-        <div style={{ flex: '1' }}>
-          <Routes>
-            <Route path="/" element={
-              <div>
-                <div className="hero-container position-relative">
-      <img src="/logo.png" alt="Logo de la Clínica" className="logo" />
-      <div className="overlay-content text-center">
-        <h1>Clínica Veterinaria Por Ellos</h1>
-        <p>Bienvenidos a Clínica Veterinaria Por Ellos, donde el bienestar de tu mascota es nuestra misión. Cada mascota que atendemos es tratada con la dedicación y el cariño que se merece. Reserva tu cita aquí y confía en nuestro equipo para mantener a tu mascota sana y feliz. Tu tranquilidad es nuestra prioridad.</p>
-        <Link to="/cita-previa" className="btn btn-primary custom-btn">Pide cita online</Link>
-      
-        <Link to="/footer.js" className="btn btn-secondary custom-btn">Cita telefónica</Link>
-
+      <ScrollToTop>
+        <div style={{ backgroundColor: '#BCCCE1', minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+          <NavBar>
+            <NavLink to="/" className="nav-link" activeClassName="active" exact>Inicio</NavLink>
+            <NavLink to="/servicios" className="nav-link" activeClassName="active">Nuestros Servicios</NavLink>
+            <NavLink to="/Peluqueria" className="nav-link" activeClassName="active">Peluquería</NavLink>
+          </NavBar>
+          <div style={{ flex: '1' }}>
+            <Routes>
+              <Route path="/" element={
+                <div>
+                  <div className="hero-container position-relative">
+                    <img src="/logo.png" alt="Logo de la Clínica" className="logo" />
+                    <div className="overlay-content text-center">
+                      <h1>Clínica Veterinaria Por Ellos</h1>
+                      <p>Bienvenidos a Clínica Veterinaria Por Ellos, donde el bienestar de tu mascota es nuestra misión. Cada mascota que atendemos es tratada con la dedicación y el cariño que se merece. Reserva tu cita aquí y confía en nuestro equipo para mantener a tu mascota sana y feliz. Tu tranquilidad es nuestra prioridad.</p>
+                      <Link to="/cita-previa" className="btn btn-primary custom-btn">Pide cita online</Link>
+                      <Link to="/footer.js" className="btn btn-secondary custom-btn">Cita telefónica</Link>
+                    </div>
                   </div>
+                  <Services />
+                  <OurTeam />
                 </div>
-                <Services />
-                <OurTeam />
-              </div>
-            } />
-            <Route path="/servicios" element={<Services />} />
-            <Route path="/cita-previa" element={<CitaPrevia />} />
-            <Route path="/urgencias" element={<Urgencias />} />
-            <Route path="/Peluqueria" element={<Peluqueria />} />
-          </Routes>
+              } />
+              <Route path="/servicios" element={<Services />} />
+              <Route path="/cita-previa" element={<CitaPrevia />} />
+              <Route path="/urgencias" element={<Urgencias />} />
+              <Route path="/Peluqueria" element={<Peluqueria />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
--- src/components/Services.js
+++ src/components/Services.js
@@ -11,21 +11,18 @@ function Services() {
           <img src="/consultas.png" alt="Servicio 1" className="img-fluid" />
           <div className="service-description">
             <p>Consultas generales y especializadas para el cuidado de tu mascota.</p>
-            <button className="btn btn-primary">Ver Más</button>
           </div>
         </div>
         <div className="col-md-4 mb-4 text-center">
           <img src="/prevencion.png" alt="Servicio 2" className="img-fluid" />
           <div className="service-description">
             <p>Programas de prevención y vacunas para mantener a tu mascota sana.</p>
-            <button className="btn btn-primary">Ver Más</button>
           </div>
         </div>
         <div className="col-md-4 mb-4 text-center">
           <img src="/cirugia.png" alt="Servicio 3" className="img-fluid" />
           <div className="service-description">
             <p>Cirugías con el mejor equipo médico para tu mascota.</p>
-            <button className="btn btn-primary">Ver Más</button>
           </div>
         </div>
       </div>
@@ -43,7 +40,9 @@ function Services() {
           <img src="/peluqueria.png" alt="Servicio 5" className="img-fluid" />
           <div className="service-description">
             <p>Peluquería para Mascotas Dales a tus mascotas el cuidado y estilo que merecen.</p>
+            <Link to="/Peluqueria">
             <button className="btn btn-primary">Pide Cita</button>
+            </Link>
           </div>
         </div>
         <div className="col-md-4 mb-4 text-center">
